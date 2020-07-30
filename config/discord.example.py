9342412cd07534fd339b9382fd058b3691889535
naoufu <naoufuf@gmail.com>
2020-07-30 13:35:33 +0100

    new config

--- config/discord.example.py
+++ config/discord.example.py
@@ -11,25 +11,22 @@ DISCORD_TOKEN = 'REPLACEME'
 
 DISCORD_CREDENTIALS = DiscordApiCredentials(token=DISCORD_TOKEN)
 
-DISCORD_USERNAME = 'SomeBot#1234'
+DISCORD_USERNAME = 'YourDiscordBOT#6969'
+
+# Prefix for the bot to recognise when you're talking to it, other the @bot mention and auto_talk channels
+CHATTER_PREFIX = ['']
 
 # Talk normally to the bot like you're conversing, without any prefix or ping
 # This requires a channel name, you can add multiple auto talk channels with a comma.
-DISCORD_AUTO_TALK = []
+DISCORD_AUTO_TALK = ['']
 
 # Learn from all servers and channels
 # If you have a large server, it will learn from bot channels too, if you enable this be sure to disable the bot from reading the channel.
 DISCORD_LEARN_FROM_ALL = False
 
-# Don't learn from any of these channels
-DISCORD_LEARN_CHANNEL_EXCEPTIONS = []
-
 # Learn from any of these channels
 # If this channel is set, it will ignore DISCORD_LEARN_FROM_ALL setting and will explicitly learn in the channel.
-DISCORD_LEARN_CHANNEL = []
-
-# If you want learned messages to be printed in the console
-RECEIPT_LOGS = False
+DISCORD_LEARN_CHANNEL = ['']
 
 # Learn from direct messages
 DISCORD_LEARN_FROM_DIRECT_MESSAGE = False
@@ -38,11 +35,16 @@ DISCORD_LEARN_FROM_DIRECT_MESSAGE = False
 # This should be set to a string containing a username like "SomeGuy#1234"
 DISCORD_LEARN_FROM_USER = None
 
+# Don't learn from any of these channels
+DISCORD_LEARN_CHANNEL_EXCEPTIONS = []
+
 # --- Technical Stuff Section ---
 # -------------------------------
 
 DISCORD_REMOVE_URL = True
 EMOTES_SKIP = True
+# If you want learned messages to be printed in the console
+RECEIPT_LOGS = False
 
 # Store training data here
 DISCORD_TRAINING_DB_PATH = 'db/discord.db'
