473a384b699d790cb34e94b52b83664157a744e8
naoufu <naoufuf@gmail.com>
2020-11-08 15:51:29 +0000

    strips punctuations from messages

--- common/discord.py
+++ common/discord.py
@@ -11,6 +11,11 @@ class DiscordHelper(object):
             filtered_content = re.sub(r'<(a?):([A-Za-z0-9_]+):([0-9]+)>', '', filtered_content)
             filtered_content = filtered_content.strip()
 
+        # Filters punctuations away for better or worse, it won't respond to random punctuations
+        if filtered_content.startswith:
+            filtered_content = re.sub(r'[^\w\s]', '', filtered_content)
+            filtered_content = filtered_content.strip()
+
         for mention in message.mentions:
             try:
                 if mention.nick is not None:
